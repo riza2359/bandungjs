8d77554497ac939ba4cec7e83a0c5575ad3b164a
Riza Saputra <riza.saputra@2359media.com>
2017-12-07 18:02:51 +0700

    feat: blueprint API

---
+++ api/controllers/PersonController.js
@@ -0,0 +1,13 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side logic for managing people
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	test(req, res) {
+        res.json({ status: 'ok' });
+    }
+};
+
